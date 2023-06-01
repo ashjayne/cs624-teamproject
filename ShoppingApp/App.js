e53cea45496fbc8ae7c6358ad80992554ee8ceb9
Ashley <48609531+ashjayne@users.noreply.github.com>
2023-06-01 03:38:04 +0000

    Finished Product creation

--- ShoppingApp/App.js
+++ ShoppingApp/App.js
@@ -1,20 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// ex01 - React Native Tutorial - Networking
+import {useEffect, useState, Component} from 'react';
+import {ActivityIndicator, FlatList, Text, View, Image, StyleSheet, Platform} from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getSuperheroes = async () => {
+    try {
+      const response = await fetch('https://raw.githubusercontent.com/ashjayne/cs624_API/main/API_test.json');
+      const json = await response.json();
+      setData(json.products);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getSuperheroes();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex: 1, padding: 24, paddingTop: 40}}>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <FlatList
+          data={data}
+          keyExtractor={({name}) => name}
+          renderItem={({item}) => (
+            <View style={styles.container}>
+              <View style={styles.productContainer}>
+                <Text>
+                  {item.name} {'\n'}
+                  by {item.author} {'\n'}
+                  Published {item.publishDate} {'\n'}
+                  Published by {item.publisher} {'\n'}
+                  ${item.price} {'\n'}
+                </Text>
+                  <Image source={{uri: item.imgURL}}
+                    style={styles.productImage} />
+              </View>
+            </View>
+          )}
+        />
+      )}
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10
   },
-});
+  productContainer: {
+    alignItems: 'center',
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 5,
+    paddingRight: 5,
+    borderWidth: 2,
+    borderRadius: 20,
+    width: 200,
+  },
+  productImage: {
+    width: 100,
+    height: 100
+  }
+})
+
+export default App;
