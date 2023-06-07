4cd9fc482cc350c807fa8bebe515010f31beaba8
hemarav98 <115053304+hemarav98@users.noreply.github.com>
2023-06-07 04:56:19 +0000

    updated cart page

--- ShoppingApp/components/Cart/Cart.js
+++ ShoppingApp/components/Cart/Cart.js
@@ -1,12 +1,30 @@
-import React, { useState } from 'react';
-import { View, FlatList, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, FlatList, Text, View, Image, StyleSheet, TouchableOpacity } from 'react-native';
 import CartButton from './CartButton';
 import CartPage from './CartPage';
 import CartTotal from './CartTotal';
 
 const Cart = () => {
+  const [data, setData] = useState([]);
   const [cartItems, setCartItems] = useState([]);
-  const [data, isCartVisible, setCartVisible] = useState(false);
+  const [isCartVisible, setCartVisible] = useState(false);
+  const [isLoading, setLoading] = useState(false);
+
+  const getSuperheroes = async () => {
+    try {
+      const response = await fetch('https://raw.githubusercontent.com/ashjayne/cs624_API/main/API_test.json');
+      const json = await response.json();
+      setData(json.products);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getSuperheroes();
+  }, []);
 
   const handleAddToCart = (item) => {
     setCartItems([...cartItems, item]);
@@ -31,30 +49,34 @@ const Cart = () => {
       ) : (
         <View>
           <CartButton onPress={handleToggleCart} />
-          <FlatList
-            data={data}
-            keyExtractor={({ name }) => name}
-            renderItem={({ item }) => (
-              <View style={styles.container}>
-                <View style={styles.productContainer}>
-                  <Text>
-                    {item.name} {'\n'}
-                    by {item.author} {'\n'}
-                    Published {item.publishDate} {'\n'}
-                    Published by {item.publisher} {'\n'}
-                    ${item.price} {'\n'}
-                  </Text>
-                  <Image source={{ uri: item.imgURL }} style={styles.productImage} />
-                  <TouchableOpacity
-                    style={styles.addToCartButton}
-                    onPress={() => handleAddToCart(item)}
-                  >
-                    <Text style={styles.addToCartButtonText}>Add to Cart</Text>
-                  </TouchableOpacity>
+          {isLoading ? (
+            <ActivityIndicator />
+          ) : (
+            <FlatList
+              data={data}
+              keyExtractor={({ name }) => name}
+              renderItem={({ item }) => (
+                <View style={styles.container}>
+                  <View style={styles.productContainer}>
+                    <Text>
+                      {item.name} {'\n'}
+                      by {item.author} {'\n'}
+                      Published {item.publishDate} {'\n'}
+                      Published by {item.publisher} {'\n'}
+                      ${item.price} {'\n'}
+                    </Text>
+                    <Image source={{ uri: item.imgURL }} style={styles.productImage} />
+                    <TouchableOpacity
+                      style={styles.addToCartButton}
+                      onPress={() => handleAddToCart(item)}
+                    >
+                      <Text style={styles.addToCartButtonText}>Add to Cart</Text>
+                    </TouchableOpacity>
+                  </View>
                 </View>
-              </View>
-            )}
-          />
+              )}
+            />
+          )}
         </View>
       )}
       {isCartVisible && <CartTotal total={calculateTotal()} />}
@@ -95,5 +117,6 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
 });
+
 
 export default Cart;
